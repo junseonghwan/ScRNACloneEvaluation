0dd0736172dc0410c623767c8042172299b2567b
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-06-10 15:57:03 -0700

    Run document().

--- Example.R
+++ Example.R
@@ -12,6 +12,6 @@ ssm <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/genotype_
 vafs <- ssm$b/ssm$d
 BScite_output <- "/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/bscite/bscite.matrices"
 cluster_labels <- VAFclusterEM::GetClones(vafs, BScite_output)
-
+VAFclusterEM::ExtractChains
 truth <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/cluster_labels.txt", sep=",", header=F)
 vmeasure(x = truth$V2, y = cluster_labels, B = 1)
