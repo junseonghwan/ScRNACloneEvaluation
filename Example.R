5f5f62ec59c04946f9a6b40c506d1a8974c1221d
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-06-10 15:42:08 -0700

    Removing 2 from the package name.

--- Example.R
+++ Example.R
@@ -3,15 +3,15 @@ library(devtools)
 library(dplyr)
 library(sabre)
 library(Rcpp)
-library(VAFclusterEM)
-#devtools::install_github("junseonghwan/VAFclusterEM")
+library(VAFclusterEM2)
+#devtools::install_github("junseonghwan/VAFclusterEM", force=TRUE)
 #load_all()
 
 # Cluster the data points by chain.
 ssm <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/genotype_ssm.txt", header=T)
 vafs <- ssm$b/ssm$d
 BScite_output <- "/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/bscite/bscite.matrices"
-cluster_labels <- GetClones(vafs, BScite_output)
+cluster_labels <- VAFclusterEM2::GetClones(vafs, BScite_output)
 
 truth <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/cluster_labels.txt", sep=",", header=F)
 vmeasure(x = truth$V2, y = cluster_labels, B = 1)
