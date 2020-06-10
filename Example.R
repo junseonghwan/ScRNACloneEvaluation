72e08158d2cdd1265d5a9b3aa8c87ebbe279fa08
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-06-10 15:53:34 -0700

    Adding back RcppExports.R

--- Example.R
+++ Example.R
@@ -3,7 +3,7 @@ library(devtools)
 library(dplyr)
 library(sabre)
 library(Rcpp)
-library(VAFclusterEM2)
+library(VAFclusterEM)
 #devtools::install_github("junseonghwan/VAFclusterEM", force=TRUE)
 #load_all()
 
@@ -11,7 +11,7 @@ library(VAFclusterEM2)
 ssm <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/genotype_ssm.txt", header=T)
 vafs <- ssm$b/ssm$d
 BScite_output <- "/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/bscite/bscite.matrices"
-cluster_labels <- VAFclusterEM2::GetClones(vafs, BScite_output)
+cluster_labels <- VAFclusterEM::GetClones(vafs, BScite_output)
 
 truth <- read.table("/Users/seonghwanjun/data/binary_cn/case5/sim0/rep19/cluster_labels.txt", sep=",", header=F)
 vmeasure(x = truth$V2, y = cluster_labels, B = 1)
