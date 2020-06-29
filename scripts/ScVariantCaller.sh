03a3535cef1e736210aded0333aa730a83d5f1e7
Seong-Hwan Jun <x_seoju@tetralith2.nsc.liu.se>
2020-06-29 10:36:43 +0200

    Working scripts for calling variants on sc data.

--- scripts/ScVariantCaller.sh
+++ scripts/ScVariantCaller.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2020-5-280
 #SBATCH -n 1
 #SBATCH -t 12:00:00
-#SBATCH -J AlleleCounts
+#SBATCH -J ScVariantCaller
 #SBATCH --mem 8G
 
 echo "$(date) Running on: $(hostname)"
@@ -11,9 +11,6 @@ echo "$(date) Running on: $(hostname)"
 BAM_FILE=$1
 OUTPUT_PATH=$2
 
-/home/x_seoju/bcftools-1.10.2/bcftools mpileup \
-	-Ou \
-	-f /proj/sc_ml/Tirosh/reference/GRCh37/Ensembl/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa \
-	${BAM_FILE_NAME} | bcftools call -P 0.1 -mv -Oz -o ${OUTPUT_PATH}.vcf.gz
+/home/x_seoju/bcftools-1.10.2/bcftools mpileup -Ou -f /proj/sc_ml/Tirosh/reference/GRCh37/Ensembl/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa ${BAM_FILE} | /home/x_seoju/bcftools-1.10.2/bcftools call -P 0.1 -mv -Ov -o ${OUTPUT_PATH}.vcf
 
 echo "$(date) Finished variants."
