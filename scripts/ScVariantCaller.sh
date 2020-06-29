31cab350bbfaee030a421c748158f6ddd4cc0d16
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-06-29 00:08:56 -0700

    Single call variant caller.

---
+++ scripts/ScVariantCaller.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2020-5-280
+#SBATCH -n 1
+#SBATCH -t 12:00:00
+#SBATCH -J AlleleCounts
+#SBATCH --mem 8G
+
+echo "$(date) Running on: $(hostname)"
+
+BAM_FILE=$1
+OUTPUT_PATH=$2
+
+bcftools mpileup \
+	-Ou \
+	-f /proj/sc_ml/Tirosh/reference/GRCh37/Ensembl/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa \
+	${BAM_FILE_NAME}.bam | bcftools call -P 0.1 -mv -Oz -o ${OUTPUT_PATH}.vcf.gz
+
+echo "$(date) Finished variants."
