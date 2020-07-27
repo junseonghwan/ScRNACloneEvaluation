959d982d57216f86668c18121aca57ecc7680e7d
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-07-27 00:26:13 -0700

    PhyloWGS script.

--- simulated/Cherry/RunPhyloWGS.sh
+++ simulated/Cherry/RunPhyloWGS.sh
@@ -3,7 +3,7 @@
 #module load Python/3.7.0-anaconda-5.3.0-extras-nsc1
 module load Python/2.7.14-nsc1-gcc-2018a-eb
 
-source activate py27
+conda activate py27
 wait
 
 python phylowgs/evolve.py $1 $2 -O $3 -B $4 -s $5 -i $6 -r $7
