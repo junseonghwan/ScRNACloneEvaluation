726b25ea6eaaae5bfda602f64c63e9f1a46cc86c
Seong-Hwan Jun <s2jun.uw@gmail.com>
2020-08-06 13:25:17 -0700

    Fix ddClone script.

--- simulated/Binary/RunDDClone.py
+++ simulated/Binary/RunDDClone.py
@@ -12,9 +12,9 @@ PATH_TO_EXECUTABLE  =  "RunDDClone.sh"
 parser = argparse.ArgumentParser(description='Script to batch run simulation studies.')
 parser.add_argument('case_path', help="Path to simulation case.", type=str)
 parser.add_argument('--sbegin', help="Sim begin.", type=int, default = 0)
-parser.add_argument('--send', help="Sim end (not inclusive).", type=int, default = 5)
+parser.add_argument('--send', help="Sim end (not inclusive).", type=int, default = 10)
 parser.add_argument('--rbegin', help="Rep begin.", type=int, default = 0)
-parser.add_argument('--rend', help="Rep end (not inclusive).", type=int, default = 20)
+parser.add_argument('--rend', help="Rep end (not inclusive).", type=int, default = 10)
 parser.add_argument('-m', '--mcmc', help="Number of MCMC iterations.", type=int, default = 10000)
 args = parser.parse_args()
 
@@ -31,12 +31,11 @@ for sim_no in range(SIM_BEGIN, SIM_END):
 		REP_PATH = SIMUL_PATH + "/rep" + str(rep_no) + "/"
 		rep_seed = np.random.randint(100000000)
 
-		# Run Rscript to generate input for B-SCITE.
 		run_command = "sbatch "
 		run_command += PATH_TO_EXECUTABLE + " "
 		run_command += str(rep_seed) + " "
 		run_command += REP_PATH + " "
 		run_command += str(args.mcmc) + " "
 		run_command += str(SC_VAR_THRESHOLD)
-		os.system(run_command)
-		#print(run_command)
+		#os.system(run_command)
+		print(run_command)
